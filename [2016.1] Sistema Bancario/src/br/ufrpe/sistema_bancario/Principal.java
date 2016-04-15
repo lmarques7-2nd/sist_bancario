f6e982072d4d3a9305b84792d7222140a6d4b533
lmarques7 <leandro.marques@gmail.com>
2016-04-15 10:20:52 -0300

    Commit da classe no repo local

---
+++ [2016.1] Sistema Bancario/src/br/ufrpe/sistema_bancario/Principal.java
@@ -0,0 +1,27 @@
+package br.ufrpe.sistema_bancario;
+
+import br.ufrpe.sistema_bancario.beans.Conta;
+import br.ufrpe.sistema_bancario.negocio.ControladorContas;
+
+public class Principal {
+
+  public static void main(String[] args) {
+    ControladorContas controlador = new ControladorContas();
+    
+    controlador.cadastrar(new Conta("1234-5", 500));
+    controlador.cadastrar(new Conta("444-5", 500));
+    controlador.cadastrar(new Conta("00015-9", 500));
+    controlador.cadastrar(new Conta("8889-7", 500));
+    
+    controlador.transferir("1234-5", "8889-7", 250);
+    
+    Conta c = controlador.procurar("1234-5");
+    System.out.println(c);
+    
+    controlador.remover("444-5");
+    controlador.remover("000777-8");
+    
+    
+  }
+
+}
